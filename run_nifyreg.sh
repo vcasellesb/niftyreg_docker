3475440a30460ab5626e932e075604282f4f5d06
Vicent Caselles <vcasellesb@uoc.edu>
2025-05-08 16:00:23 +0200

    lil script

---
+++ run_nifyreg.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+ref=$1
+flo=$2
+
+docker container run --rm \
+    -v $(dirname $ref):/ref \
+    -v $(dirname $flo):/flo vcasellesb/niftyreg \
+    bash -c "reg_aladin -ref /ref/$(basename $ref) -flo /flo/$(basename $flo) -aff /flo/aff_trans.txt && \
+    reg_resample -ref /ref/$(basename $ref) -flo /flo/$(basename $flo) -trans /flo/aff_trans.txt -res /flo/res.nii.gz"
